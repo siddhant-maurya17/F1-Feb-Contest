f8da514a34b9f701ea7dd6cdb991c5e577615fbb
Siddhant Maurya <88020811+sidhthecoder@users.noreply.github.com>
2023-02-11 11:23:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,20 +8,49 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+   arr
+    .filter(function(student){
+        return student.marks>=50;
+    })
+     
+    .map(function(student){
+      console.log(student);
+      });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(function(student){
+        if(student.marks>=50){
+        console.log(student);
+    }
+    });
 }
 
 function addData() {
   //Write your code here, just console.log
+  arr.push({id:4,name:"susan",age:"20",marks:45})
+    console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  for (let i=0;i<arr.length;i++) {
+        if (arr[i].marks < 50){
+          arr.splice(i, 1);
+          i--;
+        }
+      }
+      console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+   let newArr = [
+        { id: 4, name: "david", age: "18", marks: 80 },
+        { id: 5, name: "jessica", age: "20", marks: 85 },
+        { id: 6, name: "michael", age: "19", marks: 35 },
+      ];
+      let concatenatedArray = arr.concat(newArr);
+      console.log(concatenatedArray);
 }
